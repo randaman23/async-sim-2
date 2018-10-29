9c85e0f4c5c7f21b2701e5ca2b8cc2c702f43551
randaman23 <randaman234@gmail.com>
2018-10-29 14:48:02 -0600

    routes set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import {HashRouter, Route, Switch} from 'react-router-dom'
 import Auth from "./components/Auth";
 import Dashboard from "./components/Dashboard";
 import OneWizard from "./components/OneWizard";
@@ -10,7 +11,21 @@ import FiveWizard from "./components/FiveWizard";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div>
+        <HashRouter>
+          <Switch>
+            <Route path="/" exact component={Auth}/>
+            <Route path="/dashboard" component={Dashboard}/>
+            <Route path="/wizard/1" component={OneWizard}/>
+            <Route path="/wizard/2" component={TwoWizard}/>
+            <Route path="/wizard/3" component={ThreeWizard}/>
+            <Route path="/wizard/4" component={FourWizard}/>
+            <Route path="/wizard/5" component={FiveWizard}/>
+          </Switch>
+        </HashRouter>
+      </div>
+    )
   }
 }
 
--- src/components/ThreeWizard.js
+++ src/components/ThreeWizard.js
@@ -1,4 +1,4 @@
-import React. {Component} from 'react'
+import React, {Component} from 'react'
 
 export default class ThreeWizard extends Component{
     render(){
