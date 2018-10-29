860ea2b886f6d9fd8985430187bca2f429b80614
randaman23 <randaman234@gmail.com>
2018-10-29 14:38:40 -0600

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Auth from "./components/Auth";
+import Dashboard from "./components/Dashboard";
+import OneWizard from "./components/OneWizard";
+import TwoWizard from "./components/TwoWizard";
+import ThreeWizard from "./components/ThreeWizard";
+import FourWizard from "./components/FourWizard";
+import FiveWizard from "./components/FiveWizard";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
