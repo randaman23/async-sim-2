26e15ebe079c2af58c8c6550c8522917200f4c26
randaman23 <randaman234@gmail.com>
2018-10-30 14:34:03 -0600

    working on setting up login on auth component

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleUsername = e => {
+    this.setState({ username: e.target.value });
+  };
+
+  handlePassword = e => {
+    this.setState({ password: e.target.value });
+  };
+
+  handleLogin = () => {
+    const { username, password } = this.state;
+    axios.post(`/api/auth/login`, { username, password }).then();
+  };
+
   render() {
+    // console.log(this.state)
     return (
       <div>
         <h1>Auth</h1>
         <h2>Username</h2>
-        <input type="text" />
+        <input type="text" onChange={this.handleUsername} />
         <h2>Password</h2>
-        <input type="text" />
+        <input type="text" onChange={this.handlePassword} />
         <Link to="/dashboard">
-          <button>Login</button>
+          <button onClick={this.handleLogin}>Login</button>
         </Link>
         <button>Register</button>
       </div>
