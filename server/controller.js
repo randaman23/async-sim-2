f3d9b7dd7654a36dafeef5d701dc704634bd092a
randaman23 <randaman234@gmail.com>
2018-10-31 12:24:07 -0600

    sessions and authorizing login

--- server/controller.js
+++ server/controller.js
@@ -3,7 +3,16 @@ module.exports = {
     const db = req.app.get("db");
     const { username, password } = req.body;
     db.auth_login([username, password])
-      .then(console.log("Hoi", res.data))
+      .then(user => {
+        console.log("Hoi", user);
+        for (let i = 0; i < user.length; i++) {
+          if (user[i].username && user[i].password) {
+            req.session = req.session.user[i];
+          } else {
+            res.sendStatus(404).send("Register");
+          }
+        }
+      })
       .catch(err => {
         console.log(err);
         res.status(500).send(err);
