3884b5bffd821c2d1d62ad44ad5c1938958b3877
randaman23 <randaman234@gmail.com>
2018-11-08 16:17:29 -0700

    add name redux method

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,8 +1,12 @@
 const initialState = {
-    users: {},
-    houses: []
+   houses: []
 }
 
+const ADD_NAME_DESCRIPTION = 'reducer/ADD_NAME_DESCRIPTION'
+
+
 export default function reducer(state = initialState, action) {
-    switch(action.type) {}
+    switch(action.type) {
+
+    }
 }
